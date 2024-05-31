8ba5259c9ff87190587d297ea2237accddbb3597
flarenix <tangshenghao2001@foxmail.com>
2024-06-01 04:19:37 +0800

    adding eigen

--- src/process/process.cpp
+++ src/process/process.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include "process.h"
+#include <Eigen/Dense>
 
 using std::cout, std::endl;
+using Eigen::MatrixXd;
 
 void Process::planProcess(){
 
     cout << "this is planning process" << endl;
     my_map.mapInfo();
-    cout << "planning success!" << endl;
 
+    MatrixXd m(2, 2);
+    m(0, 0) = 3;
+    m(1, 0) = 5;
+    m(1, 1) = 1;
+    m(0, 1) = 0;
+
+    cout << m << endl;
+
+    cout << "planning success!" << endl;
 }
